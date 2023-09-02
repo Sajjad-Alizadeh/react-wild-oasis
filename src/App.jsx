904dac61c1c7d8aac076e7f25561109f3eb2988f
Sajjad Alizadeh <sajjadalizzh@gmail.com>
2023-09-02 12:50:40 +0330

    Add styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { styled } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  background-color: red;
+`;
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Heading as="h1">h1. Hello world...</Heading>
+
+          <Heading as="h2">h2. Hello world...</Heading>
+
+          <Heading as="h3">h3. Hello world...</Heading>
+
+          <Heading>default. Hello world...</Heading>
+        </Row>
+        <Row>
+          <Button>Click me...</Button>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
-export default App
+export default App;
