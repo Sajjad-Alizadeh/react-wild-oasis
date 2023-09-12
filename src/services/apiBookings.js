789cea14bc179a876ebd8b4e036f585cc702f2e7
Sajjad Alizadeh <sajjadalizzh@gmail.com>
2023-09-12 13:25:44 +0330

    Add sort & filter features for bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,6 +1,38 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
+    .from("bookings")
+    .select(
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
+    );
+
+  if (filter) {
+    for (const index in filter) {
+      query = query[filter[index].method](
+        filter[index].field,
+        filter[index].value
+      );
+    }
+  }
+
+  if (sortBy) {
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+  }
+
+  const { data, error } = await query;
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not be loaded");
+  }
+
+  return data;
+}
+
 export async function getBooking(id) {
   const { data, error } = await supabase
     .from("bookings")
